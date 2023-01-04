dd3a3b60da4855bd26092c65183f58d6c25318aa
silanurs <sarii.silanur@gmail.com>
2023-01-04 10:59:42 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const addButton = document.querySelector('.add');
-
+const total = document.querySelector('.total')
 const form = document.querySelector('form');
 const overlay = document.querySelector('.overlay');
 const closeForm = document.querySelector('.close');
@@ -11,7 +11,7 @@ const main = document.querySelector('main');
 
 submit.addEventListener('click', addBooktoLibrary)
 let myLibrary =[];
-
+let totalBooks = 0;
 function Book(title, author, pages, language, date, status){
     this.title=title;
     this.author=author;
@@ -28,10 +28,14 @@ function addBooktoLibrary(){
     let newBook = new Book(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, 
     document.getElementById("language").value, document.getElementById("date").value, document.getElementById("status").value)
     myLibrary.push(newBook)
+    totalBooks = myLibrary.length;
+    total.textContent = `${totalBooks}`
     console.log(myLibrary)
     form.classList.remove('active')
     overlay.classList.remove('active')
-    for(let i=0; i < myLibrary.length; i++){
+    newLibrary = myLibrary.slice(myLibrary.length-1);
+    console.log(newLibrary)
+    for(let i=0; i < newLibrary.length; i++){
         let div = document.createElement('div');
         div.classList.add('book');
         let x = document.createElement('p');
@@ -39,41 +43,35 @@ function addBooktoLibrary(){
         x.textContent="x";
         div.append(x)
         let title =document.createElement('h3');
-        title.textContent = myLibrary[i].title;
+        title.textContent = newLibrary[i].title;
         div.append(title)
         let author = document.createElement('p');
-        author.textContent = "Author:" + myLibrary[i].author;
+        author.textContent = "Author: " + newLibrary[i].author;
         div.append(author);
         let pages = document.createElement('p');
-        pages.textContent = "Pages: " + myLibrary[i].pages;
+        pages.textContent = "Pages: " + newLibrary[i].pages;
         div.append(pages);
         let language = document.createElement('p');
-        language.textContent = "Language: " + myLibrary[i].language;
+        language.textContent = "Language: " + newLibrary[i].language;
         div.append(language);
         let date = document.createElement('p');
-        date.textContent = "Date: " + myLibrary[i].date;
+        date.textContent = "Date: " + newLibrary[i].date;
         div.append(date);
         main.append(div);
-        if(myLibrary[i].status === "yes"){
-            div.style.backgroundImage = "linear-gradient(70deg,#eaea7f, #fdaf75)";
+        if(newLibrary[i].status === "yes"){
+            div.style.backgroundImage = "linear-gradient(70deg,#ffeddb, #e3caa5)";
         } else {
             div.style.backgroundImage = "linear-gradient(70deg,#f7ccac, #c69b7b)";
         }
         const removeBtns = document.querySelectorAll('.remove');
-        const books = document.querySelectorAll('.book');
-        removeBtns.forEach(btn => btn.addEventListener('click', ()=> {books.forEach(book =>{book.style.display ="none"})}))
+        //const books = document.querySelectorAll('.book');
+        removeBtns.forEach(btn => btn.addEventListener('click', ()=> btn.parentNode.style.display = "none"))
         
      }
      form.reset();
     
 }
-//const books = document.querySelectorAll('.book');
-//const removeBtns = document.querySelectorAll('.remove');
-//removeBtns.forEach(button => button.addEventListener('click', removeBook))
-/*function removeBook(){
-    
-    books.forEach(book => book.style.display="none")
-}*/
+
 closeForm.addEventListener('click', deleteForm);
 addButton.addEventListener('click', createForm);
 
