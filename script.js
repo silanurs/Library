655e6d86f191a74e49ac2a70a3d5e3bfc28cfe76
silanurs <sarii.silanur@gmail.com>
2023-01-04 23:22:46 +0300

    form validation

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+
 const addButton = document.querySelector('.add');
 const total = document.querySelector('.total')
 const form = document.querySelector('form');
 const overlay = document.querySelector('.overlay');
 const closeForm = document.querySelector('.close');
 const submit = document.querySelector('.submit');
-
+const warnings = document.querySelectorAll('.warning')
 const inputs = document.querySelectorAll('.input');
 const main = document.querySelector('main');
 
+String.prototype.capitalize = function () {
+    return this.toLowerCase()
+      .split(' ')
+      .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+      .join(' ');
+  };
 
 submit.addEventListener('click', addBooktoLibrary)
 let myLibrary =[];
@@ -24,17 +31,18 @@ function Book(title, author, pages, language, date, status){
 
 let newLibrary = [];
 function addBooktoLibrary(){
-  
-    let newBook = new Book(document.getElementById("title").value, document.getElementById("author").value, document.getElementById("pages").value, 
-    document.getElementById("language").value, document.getElementById("date").value, document.getElementById("status").value)
-    myLibrary.push(newBook)
+    formValidation();
+    let newBook = new Book(document.getElementById("title").value.capitalize(), document.getElementById("author").value.capitalize(), document.getElementById("pages").value, 
+    document.getElementById("language").value.capitalize(), document.getElementById("date").value, document.getElementById("status").value)
+    myLibrary.push(newBook);
     totalBooks = myLibrary.length;
     total.textContent = `${totalBooks}`
     console.log(myLibrary)
-    form.classList.remove('active')
-    overlay.classList.remove('active')
+    setData();
+    getData()
+   
     newLibrary = myLibrary.slice(myLibrary.length-1);
-    console.log(newLibrary)
+    
     for(let i=0; i < newLibrary.length; i++){
         let div = document.createElement('div');
         div.classList.add('book');
@@ -64,12 +72,39 @@ function addBooktoLibrary(){
             div.style.backgroundImage = "linear-gradient(70deg,#f7ccac, #c69b7b)";
         }
         const removeBtns = document.querySelectorAll('.remove');
-        //const books = document.querySelectorAll('.book');
-        removeBtns.forEach(btn => btn.addEventListener('click', ()=> btn.parentNode.style.display = "none"))
+        
+        removeBtns.forEach(btn => btn.addEventListener('click', (e)=> {btn.parentNode.style.display = "none";
+       
+       totalBooks--;
+        total.textContent = `${totalBooks}`
+    }))
+    
         
      }
+     
+    
      form.reset();
     
+}
+
+function setData(){
+    localStorage.setItem('books', JSON.stringify(myLibrary))
+}  
+function getData() {
+  let books =  localStorage.getItem('books');
+  books = JSON.parse(books)
+  myLibrary = books
+}
+function formValidation(){
+    let newInputs = Array.from(inputs);
+    let validatedInputs = newInputs.every(input => input.checkValidity());
+    if(validatedInputs == true) {
+        form.classList.remove('active')
+        overlay.classList.remove('active')
+    }
+    else {
+        submit.setAttribute('disabled')
+    }
 }
 
 closeForm.addEventListener('click', deleteForm);
@@ -84,4 +119,4 @@ function createForm(){
     overlay.classList.add('active');
 
 }
-   
+getData()
