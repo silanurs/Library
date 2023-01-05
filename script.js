946935963e7855e729f1eec8debc66410c2049c7
silanurs <sarii.silanur@gmail.com>
2023-01-05 13:52:15 +0300

    Add localStorage functionality

--- script.js
+++ script.js
@@ -29,72 +29,64 @@ function Book(title, author, pages, language, date, status){
   
 }
 
-let newLibrary = [];
+
 function addBooktoLibrary(){
     formValidation();
     let newBook = new Book(document.getElementById("title").value.capitalize(), document.getElementById("author").value.capitalize(), document.getElementById("pages").value, 
     document.getElementById("language").value.capitalize(), document.getElementById("date").value, document.getElementById("status").value)
-    myLibrary.push(newBook);
-    totalBooks = myLibrary.length;
-    total.textContent = `${totalBooks}`
-    console.log(myLibrary)
-    setData();
-    getData()
-   
-    newLibrary = myLibrary.slice(myLibrary.length-1);
+    myLibrary.push(newBook); 
+    total.textContent = myLibrary.length;
+    console.log(myLibrary.length)
+     setData();
+     render();
     
-    for(let i=0; i < newLibrary.length; i++){
-        let div = document.createElement('div');
+     form.reset();
+    
+}
+function createCard(book){
+    setData()
+    let div = document.createElement('div');
         div.classList.add('book');
         let x = document.createElement('p');
         x.classList.add('remove');
         x.textContent="x";
         div.append(x)
         let title =document.createElement('h3');
-        title.textContent = newLibrary[i].title;
+        title.textContent = book.title;
         div.append(title)
         let author = document.createElement('p');
-        author.textContent = "Author: " + newLibrary[i].author;
+        author.textContent = "Author: " + book.author;
         div.append(author);
         let pages = document.createElement('p');
-        pages.textContent = "Pages: " + newLibrary[i].pages;
+        pages.textContent = "Pages: " + book.pages;
         div.append(pages);
         let language = document.createElement('p');
-        language.textContent = "Language: " + newLibrary[i].language;
+        language.textContent = "Language: " + book.language;
         div.append(language);
         let date = document.createElement('p');
-        date.textContent = "Date: " + newLibrary[i].date;
+        date.textContent = "Date: " + book.date;
         div.append(date);
         main.append(div);
-        if(newLibrary[i].status === "yes"){
-            div.style.backgroundImage = "linear-gradient(70deg,#ffeddb, #e3caa5)";
-        } else {
+        if(book.status === "yes"){
             div.style.backgroundImage = "linear-gradient(70deg,#f7ccac, #c69b7b)";
+        } else {
+            div.style.backgroundImage = "linear-gradient(70deg,#594545, #9e7676)";
+            div.style.color = "#ffeddb"
         }
-        const removeBtns = document.querySelectorAll('.remove');
         
-        removeBtns.forEach(btn => btn.addEventListener('click', (e)=> {btn.parentNode.style.display = "none";
+        
+        x.addEventListener('click', () =>{myLibrary.splice(myLibrary.indexOf(book),1)
+        total.textContent = myLibrary.length;
+        setData()
+        render()
+    });
        
-       totalBooks--;
-        total.textContent = `${totalBooks}`
-    }))
-    
-        
-     }
-     
-    
-     form.reset();
-    
-}
+      
+        }
 
-function setData(){
-    localStorage.setItem('books', JSON.stringify(myLibrary))
-}  
-function getData() {
-  let books =  localStorage.getItem('books');
-  books = JSON.parse(books)
-  myLibrary = books
-}
+    
+
+
 function formValidation(){
     let newInputs = Array.from(inputs);
     let validatedInputs = newInputs.every(input => input.checkValidity());
@@ -119,4 +111,25 @@ function createForm(){
     overlay.classList.add('active');
 
 }
-getData()
+function render(){
+   const books = document.querySelectorAll('.book');
+   books.forEach(book => main.removeChild(book));
+   
+   for (let i=0; i<myLibrary.length; i++){
+    createCard(myLibrary[i]);
+}}
+function setData(){
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+}
+function restore(){
+    if(!localStorage.myLibrary){
+        render();
+    } else {
+        let objects = localStorage.getItem('myLibrary');
+        objects = JSON.parse(objects);
+        myLibrary = objects;
+        total.textContent = objects.length;
+        render();
+    }
+ }
+ restore()
